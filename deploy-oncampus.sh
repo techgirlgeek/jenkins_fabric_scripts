ee3181771917d29190a8c1e9b6b09576daebf747
Jenkins User <karyn.cassio@colorado.edu>
2015-05-29 13:35:08 -0600

    Remove bash and exit from scripts.

--- deploy-oncampus.sh
+++ deploy-oncampus.sh
@@ -2,13 +2,5 @@
 env
 
 # ssh into hdsapache0
-ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase && drush updb && drush fra -y && drush cc all && exit && bash" 
-ssh prodweb1 "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase && exit && bash" 
-
-# Move to the root of the site directory
-#cd /var/www/web/oncampus.colorado.edu/public
-#ls -al
-
-# Get a status of the current build
-#git status
-
+ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase && drush updb && drush fra -y && drush cc all" 
+ssh prodweb1 "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase"
