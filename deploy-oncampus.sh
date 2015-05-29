d24a9c3fbe64a9cd33fe7df59242c1b2e34bc73d
Jenkins User <karyn.cassio@colorado.edu>
2015-05-29 13:11:52 -0600

    Add git commands, and cache clears to update servers from master.

--- deploy-oncampus.sh
+++ deploy-oncampus.sh
@@ -2,12 +2,13 @@
 env
 
 # ssh into hdsapache0
-ssh prodweb 
+ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase && drush updb && drush fra -y && drush cc all && exit && bash" 
+ssh prodweb1 "cd /var/www/web/oncampus.colorado.edu/public && git fetch && git rebase && exit && bash" 
 
 # Move to the root of the site directory
-cd /var/www/web/oncampus.colorado.edu/public
-ls -al
+#cd /var/www/web/oncampus.colorado.edu/public
+#ls -al
 
 # Get a status of the current build
-git status
+#git status
 
