bfa0acf4c1c01479bb955c0eb6442110669d4a58
Jenkins User <karyn.cassio@colorado.edu>
2015-06-01 13:10:02 -0600

    Make sure we only have the most recent two site archives.

--- site-archive.sh
+++ site-archive.sh
@@ -2,4 +2,4 @@
 env
 
 # ssh into hdsapache0 and archive site being deployed
-ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && drush archive-dump --destination=/web/oncampus/archives/oncampus-$(date +'%Y%m%d_%H%M%S').tar"
+ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && drush archive-dump --destination=/web/oncampus/archives/oncampus-$(date +'%Y%m%d_%H%M%S').tar && cd /web/oncampus/archives && ls -t | tail -n +3 | xargs rm --"
