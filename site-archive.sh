db418fc90f3cf99a81ccbdcbab6bc7f06c97b4d5
Jenkins User <karyn.cassio@colorado.edu>
2015-06-01 09:24:18 -0600

    Add drush-backup to script.

--- site-archive.sh
+++ site-archive.sh
@@ -2,8 +2,4 @@
 env
 
 # ssh into hdsapache0 and archive site being deployed
-ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && ls -al"
-
-# ssh into hdsapache1 and archive site being deployed
-ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && ls -al"
-
+ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && drush archive-dump --destination=/web/oncampus/archives/oncampus-$(date +'%Y%m%d_%H%M%S').tar"
