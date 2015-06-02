2f57c4a235c38f1475b8d0df5798bc439faf3582
Jenkins User <karyn.cassio@colorado.edu>
2015-06-02 10:40:04 -0600

    Setup archive script to accept variables.

--- site-archive.sh
+++ site-archive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 env
+echo 'site being updated: ' $1
 
 # ssh into hdsapache0 and archive site being deployed
-ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public && drush archive-dump --destination=/web/oncampus/archives/oncampus-$(date +'%Y%m%d_%H%M%S').tar && cd /web/oncampus/archives && ls -t | tail -n +3 | xargs rm --"
+ssh prodweb "cd /var/www/web/$1.colorado.edu/public && drush archive-dump --destination=/web/$1/archives/$1-$(date +'%Y%m%d_%H%M%S').tar && cd /web/$1/archives && ls -t | head -n 1 | xargs gzip && ls -t | tail -n +3 | xargs rm --"
