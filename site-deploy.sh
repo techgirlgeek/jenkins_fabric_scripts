760c31e930566df765a801cd0e77e35682bd89a1
Jenkins User <karyn.cassio@colorado.edu>
2015-06-02 12:49:25 -0600

    Call to clear_apc.php script on servers.

--- site-deploy.sh
+++ site-deploy.sh
@@ -2,7 +2,7 @@
 env
 
 # ssh into hdsapache0 rebase codebase to master
-ssh prodweb "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && drush updb && drush fra -y && drush cc all" 
+ssh prodweb "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && drush updb && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
 # ssh into hdsapache1 rebase codebase to master
-ssh prodweb1 "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase"
+ssh prodweb1 "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
