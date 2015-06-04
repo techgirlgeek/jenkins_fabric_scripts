8fd4d55a805a6e560fe8921c859c1297b696ba36
Jenkins User <karyn.cassio@colorado.edu>
2015-06-04 16:27:43 -0600

    Add a second cache-clear to make sure site recognizes features are enabled.

--- site-deploy.sh
+++ site-deploy.sh
@@ -2,7 +2,7 @@
 env
 
 # ssh into hdsapache0 rebase codebase to master
-ssh prodweb "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && drush updb -y && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
+ssh prodweb "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
 # ssh into hdsapache1 rebase codebase to master
 ssh prodweb1 "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
