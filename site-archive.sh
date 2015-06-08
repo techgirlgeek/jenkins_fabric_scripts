c31692fa43e8ba241170f648884f3f3d99f6225d
Jenkins User <karyn.cassio@colorado.edu>
2015-06-08 10:17:51 -0600

    Hard-coded test for nrsmboulder.org
    Not optimal but should get Jenkins passing for now

--- site-archive.sh
+++ site-archive.sh
@@ -2,5 +2,11 @@
 env
 echo 'site being updated: ' $1
 
+site=$1.colorado.edu
+
+if [ $1 == "nrsmboulder.org" ]; then
+  site="nrsmboulder.org"
+fi
+
 # ssh into hdsapache0 and archive site being deployed
-ssh prodweb "cd /var/www/web/$1.colorado.edu/public && drush archive-dump --destination=/web/$1/archives/$1-$(date +'%Y%m%d_%H%M%S').tar && cd /web/$1/archives && ls -t | head -n 1 | xargs gzip && ls -t | tail -n +3 | xargs rm --"
+ssh prodweb "cd /var/www/web/$site/public && drush archive-dump --destination=/web/$1/archives/$1-$(date +'%Y%m%d_%H%M%S').tar && cd /web/$1/archives && ls -t | head -n 1 | xargs gzip && ls -t | tail -n +3 | xargs rm --"
