7fe95583eb9b103da2fcd25affb713ca8ad03d18
Jenkins User <karyn.cassio@colorado.edu>
2015-06-08 10:22:21 -0600

    Hard coded fix for nrsmboulder.org
    Should get builds passing for now.

--- site-deploy.sh
+++ site-deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -x
 env
 
+site=$1.colorado.edu
+
+if [ $1 == "nrsmboulder.org" ]; then
+  site="nrsmboulder.org"
+fi
+
 # ssh into hdsapache0 rebase codebase to master
-ssh prodweb "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
+ssh prodweb "cd /var/www/web/$site/public && git fetch && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
 # ssh into hdsapache1 rebase codebase to master
-ssh prodweb1 "cd /var/www/web/$1.colorado.edu/public && git fetch && git rebase && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
+ssh prodweb1 "cd /var/www/web/$site/public && git fetch && git rebase && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
