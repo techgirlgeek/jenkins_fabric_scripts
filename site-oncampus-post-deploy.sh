c85d0c15111b07b2fa9a851b2126d722cefbac55
Jenkins User <karyn.cassio@colorado.edu>
2015-06-19 11:24:19 -0600

    Add cc to oncampus directory.

--- site-oncampus-post-deploy.sh
+++ site-oncampus-post-deploy.sh
@@ -3,4 +3,4 @@ env
 
 site=$1.colorado.edu
 
-ssh prodweb "drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all"
+ssh prodweb "cd /var/www/web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all"
