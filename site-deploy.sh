be709bd5b9fd3fd34499c7f6a4a96517c37aadd9
Jenkins User <karyn.cassio@colorado.edu>
2015-06-19 08:25:23 -0600

    Add a cache-clear on hdsapache1.

--- site-deploy.sh
+++ site-deploy.sh
@@ -11,4 +11,4 @@ fi
 ssh prodweb "cd /var/www/web/$site/public && git fetch && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
 # ssh into hdsapache1 rebase codebase to master
-ssh prodweb1 "cd /var/www/web/$site/public && git fetch && git rebase && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
+ssh prodweb1 "cd /var/www/web/$site/public && git fetch && git rebase && drush cc all && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
