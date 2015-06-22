ba0bf45909dd503637f336fb4c05d356f3ae141d
Jenkins User <karyn.cassio@colorado.edu>
2015-06-22 16:25:24 -0600

    Staging deployment script, without features.

--- site-deploy-nonfeatures.sh
+++ site-deploy-nonfeatures.sh
@@ -7,6 +7,8 @@ if [ $1 == "nrsmboulder.org" ]; then
   site="nrsmboulder.org"
 fi
 
-# ssh into devweb rebase codebase to staging
-ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all" 
+# ssh into hdsapache0 rebase codebase to master
+ssh prodweb "cd /var/www/web/$site/public && git fetch && git rebase && drush updb -y && drush cc all && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
+# ssh into hdsapache1 rebase codebase to master
+ssh prodweb1 "cd /var/www/web/$site/public && git fetch && git rebase && drush cc all && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
