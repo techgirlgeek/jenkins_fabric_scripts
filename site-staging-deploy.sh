55bd23559ce8dfa09712b9e7b805e3d596f794c7
Jenkins User <karyn.cassio@colorado.edu>
2015-06-22 16:34:31 -0600

    Remove nrsmboulder if statement. Not necessary for staging.

--- site-staging-deploy.sh
+++ site-staging-deploy.sh
@@ -3,10 +3,6 @@ env
 
 site=$1.colorado.edu
 
-if [ $1 == "nrsmboulder.org" ]; then
-  site="nrsmboulder.org"
-fi
-
 # ssh into devweb rebase codebase to staging
 ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
 
