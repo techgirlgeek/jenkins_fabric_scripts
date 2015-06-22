972d4913eefce478bea3e2cb144f0b44e576407b
Jenkins User <karyn.cassio@colorado.edu>
2015-06-22 16:42:02 -0600

    Change name of script to match others.

--- site-staging-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-env
-
-site=$1.colorado.edu
-
-# ssh into devweb rebase codebase to staging
-ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
-
