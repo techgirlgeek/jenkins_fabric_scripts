05e439e244c68c125669eb82d73af68adf2298cb
Jenkins User <karyn.cassio@colorado.edu>
2015-06-22 15:33:20 -0600

    Add scripts for deploying staging branch to devweb.

---
+++ site-staging-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+env
+
+site=$1.colorado.edu
+
+if [ $1 == "nrsmboulder.org" ]; then
+  site="nrsmboulder.org"
+fi
+
+# ssh into devweb rebase codebase to staging
+ssh devweb "cd /web/$site/public && git fetch && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
+
