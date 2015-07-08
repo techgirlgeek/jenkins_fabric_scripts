ffd2e4fbb83c0cb479f14681f4882ce48f918829
Jenkins User <karyn.cassio@colorado.edu>
2015-07-08 13:58:17 -0600

    Remove variables from script. All hard coded information.

--- site-deploy-myroom.sh
+++ site-deploy-myroom.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -x
 env
 
-site=$1.colorado.edu
-
-if [ $1 == "nrsmboulder.org" ]; then
-  site="nrsmboulder.org"
-fi
-
-# ssh into hdsapache0 rebase codebase to master
+# ssh into hdsapache0 rebase codebase to myroom master
 ssh prodweb "cd /var/www/web/oncampus.colorado.edu/public/sites/all/modules/custom/myroom && git fetch && git rebase && drush updb -y && drush cc all && wget --no-check-certificate -O - -q https://a0.hdslc.colorado.edu/clear_apc.php" 
 
-# ssh into hdsapache1 rebase codebase to master
+# ssh into hdsapache1 rebase codebase to myroom master
 ssh prodweb1 "cd /var/www/web/oncampus.colorado.edu/public/sites/all/modules/custom/myroom && git fetch && git rebase && drush cc all && wget --no-check-certificate -O - -q https://a1.hdslc.colorado.edu/clear_apc.php"
