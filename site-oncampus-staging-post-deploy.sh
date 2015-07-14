a9571c7df3b0b1ff5468290f3153f8c96b058405
Jenkins User <karyn.cassio@colorado.edu>
2015-07-14 14:18:52 -0600

    Add reslife info as an elif and rename so that we can use the same script for all post deploy actions.

--- site-oncampus-staging-post-deploy.sh
+++ site-oncampus-staging-post-deploy.sh
@@ -3,11 +3,11 @@ env
 
 site=$1.colorado.edu
 
-if [ $1 == "oncampus.devweb.colorado.edu" ]; then
-ssh devweb "cd /web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush vset bakery_domain .$site && drush vset bakery_master 'https://$site' && drush cc all"
-fi 
+if [ $1 == "oncampus.devweb" ]; then
+	ssh devweb "cd /web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush vset bakery_domain .$site && drush vset bakery_master 'https://$site' && drush cc all"
 
-if [ $1 = 'reslife.oncampus.devweb.colorado.edu' ]; then
-master=oncampus.devweb.colorado.edu
-ssh devweb "cd /web/$site/public && drush vset bakery_domain .$master && drush vset bakery_master 'https://$master' && drush cc all"
+elif [ $1 = 'reslife.oncampus.devweb' ]; then
+	master=oncampus.devweb.colorado.edu
+
+	ssh devweb "cd /web/$site/public && drush vset bakery_domain .$master && drush vset bakery_master 'https://$master' && drush cc all"
 fi
