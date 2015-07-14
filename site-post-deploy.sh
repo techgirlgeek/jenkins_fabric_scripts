e38855353cac0bf831b326731f6b94b56f338196
Jenkins User <karyn.cassio@colorado.edu>
2015-07-14 14:55:27 -0600

    Comment out bakery information until it is pushed to production.

--- site-post-deploy.sh
+++ site-post-deploy.sh
@@ -3,4 +3,11 @@ env
 
 site=$1.colorado.edu
 
-ssh prodweb "cd /var/www/web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all"
+if [ $1 == "oncampus" ]; then
+	ssh prodweb "cd /var/www/web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all"
+#"drush vset bakery_domain .$site && drush vset bakery_master 'https://$site/' && drush cc all"
+
+#elif [ $1 == "reslife.oncampus" ]; then
+#	master=oncampus.colorado.edu
+#	ssh prodweb "cd /var/www/web/$site/public && drush vset bakery_domain .$master && drush vset bakery_master 'https://$master/' && drush cc all"
+fi
