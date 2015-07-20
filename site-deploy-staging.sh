b651ebd0f6b2fc386cd2a01909386623f2dc62b5
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 11:15:22 -0600

    Revert to previous changes.

--- site-deploy-staging.sh
+++ site-deploy-staging.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -x
 env
 
-#site=$1.colorado.edu
+site=$1.colorado.edu
 
 # ssh into devweb rebase codebase to staging
-#ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
-#cd $1/$2/$3 && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
-cd $1/$2/$3 && git fetch && git status
-#checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
-
+ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
