1c14af12e53d8cde5c0947a73f2802e07c3d7511
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 13:34:44 -0600

    Add new post deploy scripts for reslife.devweb and oncampus.devweb to work with fabric

--- fabfile.py
+++ fabfile.py
@@ -7,5 +7,11 @@ def env():
 def staging_deploy(path, site, path2):
   run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
 
+def staging_oncampus_post_deploy(slave,site):
+  run("/usr/bin/jenkins-scripts/site-oncampus-staging-post.sh % % % %" % (path, site, path2, slave))
+
+def staging_reslife_post_deploy(master,site):
+  run("/usr/bin/jenkins-scripts/site-reslife-staging-post.sh % % % %" % (path, site, path2, master))
+
 def test(path,site,path2):
   run("/usr/bin/jenkins-scripts/test.sh %s %s %s" % (path, site, path2))
---
+++ site-reslife-staging-post.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x
+	cd /$1/$2/$3 && drush vset bakery_domain .$4 && drush vset bakery_master 'https://$4/' && drush cc all
