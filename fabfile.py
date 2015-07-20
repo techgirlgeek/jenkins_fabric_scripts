f01f065125122a9cf096d6d7e60ff01134425c34
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 08:35:37 -0600

    Test fabric script for deployments.

---
+++ fabfile.py
@@ -0,0 +1,5 @@
+#!/user/bin/env python
+from fabric.api import env, run
+
+def staging_deploy(path, site path2):
+  run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
--- site-deploy-staging.sh
+++ site-deploy-staging.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 env
 
-site=$1.colorado.edu
+#site=$1.colorado.edu
 
 # ssh into devweb rebase codebase to staging
-ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
+#ssh devweb "cd /web/$site/public && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all" 
+#cd $1/$2/$3 && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
+cd $1/$2/$3 && git fetch && git status
+#checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
 
