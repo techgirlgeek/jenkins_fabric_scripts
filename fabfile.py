402e2718f34f34c6ad7e96da499060373e056ca9
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 13:41:16 -0600

    Add arguments to fabric functions

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,10 @@ def env():
 def staging_deploy(path, site, path2):
   run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
 
-def staging_oncampus_post_deploy(slave,site):
+def staging_oncampus_post_deploy(path, site, path2, slave):
   run("/usr/bin/jenkins-scripts/site-oncampus-staging-post.sh % % % %" % (path, site, path2, slave))
 
-def staging_reslife_post_deploy(master,site):
+def staging_reslife_post_deploy(path, site, path2, master):
   run("/usr/bin/jenkins-scripts/site-reslife-staging-post.sh % % % %" % (path, site, path2, master))
 
 def test(path,site,path2):
