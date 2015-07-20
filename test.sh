6a59320e85dae333923f6d67ba9a2873e1e3ba98
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 12:24:43 -0600

    Another fabric test before applying to staging builds

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-cd /$1/$2/$3 && git fetch && git status
+cd /$1/$2/$3 && git fetch && git status && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
