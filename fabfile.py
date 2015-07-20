6f3aab612cda7067b2f05d23312c25d9c3953b5b
Jenkins User <karyn.cassio@colorado.edu>
2015-07-20 09:33:36 -0600

    Add test function to fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,11 @@
 #!/user/bin/env python
 from fabric.api import env, run
 
+def env():
+  run("env")
+
 def staging_deploy(path, site, path2):
   run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
+
+def test(path,site,path2):
+  run("/usr/bin/jenkins-scripts/test.sh %s %s %s" % (path, site, path2))
