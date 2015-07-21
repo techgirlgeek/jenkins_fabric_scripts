fbc39cf6bc41be853c4c3565455e7b8ce3d740a1
Karyn Cassio <karen.cassio@colorado.edu>
2015-07-21 18:10:46 +0000

    Reformat and comment post deploy scripts for oncampus and reslife staging.

--- site-oncampus-staging-post.sh
+++ site-oncampus-staging-post.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
-cd /$1/$2/$3 && drush sqlq "INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);" && drush vset bakery_domain .$2 && drush vset bakery_master https://$2/ && php -r "print json_encode(array(\"$4\"));" | drush vset --format=json bakery_slaves -  && drush cc all
+# Change to working directory
+cd /$1/$2/$3 
+
+# MySql query to insert permissions on reslife menu item
+drush sqlq "INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);" 
+
+# Set bakery domain for SSO
+drush vset bakery_domain .$2 
+
+# Set bakery master for getting user account information for SSO
+drush vset bakery_master https://$2/ 
+
+# Set bakery slaves that will use this master for SSO
+php -r "print json_encode(array(\"$4\"));" | drush vset --format=json bakery_slaves -  && drush cc all
--- site-reslife-staging-post.sh
+++ site-reslife-staging-post.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
-	cd /$1/$2/$3 && drush vset bakery_domain .$4 && drush vset bakery_master "https://$4/" && drush cc all
+
+# Change to working directory
+cd /$1/$2/$3 
+
+# Set bakery_domain for SSO
+drush vset bakery_domain .$4 
+
+# Set bakery master for getting user accounts
+drush vset bakery_master "https://$4/" 
+
+# Clear drush cache, because drupal
+drush cc all
