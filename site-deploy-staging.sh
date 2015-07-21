0ee42d6ba5bae3d09f2700057cadd9397356ffb7
Karyn Cassio <karen.cassio@colorado.edu>
2015-07-21 17:44:26 +0000

    Clean up and comment staging deployment script.

--- site-deploy-staging.sh
+++ site-deploy-staging.sh
@@ -1,3 +1,23 @@
 #!/bin/bash -x
+
+# Change to site's working directory
+cd /$1/$2/$3 
+
+# Checkout staging if not already on that branch
+git checkout staging 
+
 # rebase codebase to staging
-cd /$1/$2/$3 && git fetch && git checkout staging && git rebase && drush updb -y && drush cc all && drush fra -y && drush cc all
+git fetch && git rebase 
+
+# Update databases
+drush updb -y 
+
+# Clear drupal cache
+drush cc all 
+
+# Revert all enabled features on the site
+drush fra -y 
+
+# Clear cache one more time, just in case
+drush cc all
+
