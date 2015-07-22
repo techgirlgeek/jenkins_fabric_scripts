e0e2d924b1ea896eebac16e08d476f92ec53674e
Karyn Cassio <karen.cassio@colorado.edu>
2015-07-22 15:44:19 +0000

    Add clear apc cache script. Update fabfile to include production sites.

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,18 @@ from fabric.api import env, run
 def env():
   run("env")
 
+def site_deploy(path, site, path2):
+  run("/usr/bin/jenkins-scripts/site-deploy.sh %s %s %s" % (path, site, path2))
+
+def site_archive():
+  run("/usr/bin/jenkins-scripts/site-clear-apc-cache.sh %s" % (site))
+
+def site_post_deploy_oncampus(path, site, path2, slave):
+   run("/usr/bin/jenkins-scripts/site-oncampus-post.sh %s %s %s %s" % (path, site, path2, slave))
+
+def site_post_deploy_oncampus(path, site, path2, slave):
+   run("/usr/bin/jenkins-scripts/site-reslife-post.sh %s %s %s %s" % (path, site, path2, master))
+
 def staging_deploy(path, site, path2):
   run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
 
---
+++ site-clear-apc-cache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x
+wget --no-check-certificate -O - -q https://$1.hdslc.colorado.edu/clear_apc.php
