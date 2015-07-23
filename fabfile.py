e3f4f4dfa3cd0d19b53d5a242d09f0e770aec054
Karyn Cassio <karen.cassio@colorado.edu>
2015-07-23 20:40:58 +0000

    Remove unused/duplicate function.

--- fabfile.py
+++ fabfile.py
@@ -22,9 +22,6 @@ def site_clear_apc(site):
 def staging_deploy(path, site, path2):
   run("/usr/bin/jenkins-scripts/site-deploy-staging.sh %s %s %s" % (path, site, path2))
 
-def staging_oncampus_post_deploy(path, site, path2, slave):
-  run("/usr/bin/jenkins-scripts/site-oncampus-staging-post.sh %s %s %s %s" % (path, site, path2, slave))
-
 def staging_reslife_post_deploy(path, site, path2, master):
   run("/usr/bin/jenkins-scripts/site-reslife-staging-post.sh %s %s %s %s" % (path, site, path2, master))
 
