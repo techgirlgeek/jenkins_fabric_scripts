eca7dddfd17d87f5824293ec59d871b22b3a33a0
Karyn Cassio <karen.cassio@colorado.edu>
2015-07-23 16:31:41 +0000

    Fix missing &&

--- site-post-deploy.sh
+++ site-post-deploy.sh
@@ -5,7 +5,7 @@ site=$1.colorado.edu
 
 if [ $1 == "oncampus" ]; then
 	slave="https://reslife.oncampus.colorado.edu/"
-	ssh prodweb "cd /var/www/web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all drush vset bakery_domain .$site && drush vset bakery_master 'https://$site/' && php -r 'print json_encode(array(\"$slave\"));' | drush vset --format=json bakery_slaves - && drush cc all"
+	ssh prodweb "cd /var/www/web/$site/public && drush sqlq 'INSERT INTO menu_links_visibility_role (mlid,rid) VALUES (2540,3),(2540,16),(2540,17),(2540,18),(2540,20),(2540,21),(2540,26),(2540,27);' && drush cc all && drush vset bakery_domain .$site && drush vset bakery_master 'https://$site/' && php -r 'print json_encode(array(\"$slave\"));' | drush vset --format=json bakery_slaves - && drush cc all"
 
 elif [ $1 == "reslife.oncampus" ]; then
 	master=oncampus.colorado.edu
